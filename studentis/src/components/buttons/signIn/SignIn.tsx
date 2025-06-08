f4c46448f0081df844d09c981775b9f3becc0545
Murad-Yusifov <muradmy-azmp203@code.edu.az>
2025-06-09 01:15:52 +0400

    login page css bitdir qaldi funksionalliq

---
+++ studentis/src/components/buttons/signIn/SignIn.tsx
@@ -0,0 +1,11 @@
+const SignIn = () => {
+  return (
+    <div>
+      <button className="w-full px-6 py-5 mb-5 text-sm font-bold leading-none text-white transition duration-300 md:w-96 rounded-2xl bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-100">
+        Sign In
+      </button>
+    </div>
+  );
+};
+
+export default SignIn;
